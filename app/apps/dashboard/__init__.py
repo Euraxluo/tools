eb6ac45e94b70b28e160de171f0c9a3c4dba5711
euraxluo <1793939176@qq.com>
2022-07-02 11:13:36 +0800

    添加 elements

---
+++ app/apps/dashboard/__init__.py
@@ -0,0 +1,7 @@
+from .card import Card
+from .pipe import Pie
+from .datagrid import DataGrid
+from .dashboard import Dashboard
+from .radar import Radar
+from .player import Player
+from .editor import Editor
---
+++ app/pages/4_🪟_Grid_Elements.py
@@ -0,0 +1,15 @@
+import streamlit as st
+
+# 导入多app 框架
+from utils.multi_app import *
+
+# 导入app列表
+from apps import elements
+
+st.set_page_config(layout="wide")
+application = multi_app()
+
+# add applications
+application.add_app('elements', elements.app)
+# run applications
+application.run()
